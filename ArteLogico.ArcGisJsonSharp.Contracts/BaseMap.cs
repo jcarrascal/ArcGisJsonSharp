8a77de3f4ccd164b424dc4ea305e39cd9b080bef
Julio César Carrascal Urquijo <jcarrascal@gmail.com>
2015-11-11 21:34:32 -0500

    Initial documentation.

--- ArteLogico.ArcGisJsonSharp.Contracts/BaseMap.cs
+++ ArteLogico.ArcGisJsonSharp.Contracts/BaseMap.cs
@@ -2,10 +2,26 @@
 
 namespace ArteLogico.ArcGisJsonSharp.Contracts
 {
+    /// <summary>
+    /// A BaseMap provides the geographic context of a web map.
+    /// http://resources.arcgis.com/en/help/arcgis-web-map-json/index.html#/baseMap/02qt00000004000000/
+    /// </summary>
     public class BaseMap
     {
+        /// <summary>
+        /// Gets or sets the base map layers.
+        /// </summary>
+        /// <value>
+        /// The base map layers.
+        /// </value>
         public List<BaseMapLayer> BaseMapLayers { get; set; }
 
+        /// <summary>
+        /// Gets or sets the title that can be used in a table of contents.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         public string Title { get; set; }
     }
 }
--- ArteLogico.ArcGisJsonSharp.Contracts/BaseMapLayer.cs
+++ ArteLogico.ArcGisJsonSharp.Contracts/BaseMapLayer.cs
@@ -1,15 +1,63 @@
 ﻿namespace ArteLogico.ArcGisJsonSharp.Contracts
 {
+    /// <summary>
+    /// A layer that provides geographic context to the map.
+    /// http://resources.arcgis.com/en/help/arcgis-web-map-json/index.html#/baseMapLayer/02qt00000003000000/
+    /// </summary>
     public class BaseMapLayer
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public string Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the basemap layer appears
+        /// on top of all operational layers (true) or beneath all operational
+        /// layers (false).
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is reference; otherwise, <c>false</c>.
+        /// </value>
         public bool IsReference { get; set; }
 
+        /// <summary>
+        /// Gets or sets the degree of opacity applied to the layer, where 0 is
+        /// full transparency and 1 is no transparency.
+        /// </summary>
+        /// <value>
+        /// The opacity.
+        /// </value>
         public int Opacity { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type. When this property is included, the url
+        /// property is not required. Acceptable values include: OpenStreetMap
+        /// | BingMapsAerial | BingMapsRoad | BingMapsHybrid.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the layer is initially
+        /// visible in the web map.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if visibility; otherwise, <c>false</c>.
+        /// </value>
         public bool Visibility { get; set; }
 
+        /// <summary>
+        /// Gets or sets the URL to the layer.
+        /// </summary>
+        /// <value>
+        /// The URL.
+        /// </value>
         public string Url { get; set; }
     }
 }
